2d8c20d15aaa8bc6f4e283ce7a4c0ccbb57267bc
Kelly Pressley <kellypressley86@gmail.com>
2020-01-12 20:24:42 -0700

    small changes to the alert div for better display

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './Header.css';
 
-function Header(props) {
-    return (
-        <header className="score">
-            <div className="row">
-                <div className="col-md-7 col-left">
-                    <h2>Click on all the GI Joes without
+const Header = (props) => (
+    <header className="score">
+        <div className="row">
+            <div className="col-md-7 col-left">
+                <h2>Click on all the G.I. Joes without
                         clicking the same one twice!</h2>
-                </div>
-                <div className="col-md-5 col-right">
-                    <h3>Your Current Score: {props.score}</h3>
-                    <br/><br/>
-                    <h3>Your Best Score: {props.topScore}</h3>
-                </div>
             </div>
-        </header>
-    );
-}
+            <div className="col-md-5 col-right">
+                <h3>Current Score: {props.score}</h3>
+                <br /><br />
+                <h3>Best Score: {props.topScore}</h3>
+            </div>
+        </div>
+    </header>
+);
 
 export default Header;
